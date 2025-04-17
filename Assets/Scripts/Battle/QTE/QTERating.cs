1f70e38e036de322603942066f81b63f1de5eab1
CrimsonWar <ntheprofessional@gmail.com>
2025-04-17 06:43:11 +0200

    starting out QTE

---
+++ Assets/Scripts/Battle/QTE/QTERating.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTERating : MonoBehaviour
+{
+    public void DisableRating()
+    {
+        gameObject.SetActive(false);
+    }
+}
